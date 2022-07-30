9f0f852594226d07cebebd46b97196d08751b879
Priyanshu <caytuspriyanshu@gmail.com>
2022-07-30 11:39:04 +0530

    Input added Output added Main code modified

--- dcs_main.cpp
+++ dcs_main.cpp
@@ -2,19 +2,6 @@
 using namespace std;
 #define map unordered_map<int,int>
 
-void printGraph(vector<vector<int>>& graph,map&revmp){
-    cout<<"\nPrint Graph\n";
-    int ptr = 0;
-    for(auto x : graph){
-        cout<<revmp[ptr]<<" :: ";
-        for(auto y : x){
-            cout<<revmp[y]<<" ";
-        }
-        cout<<"\n";
-        ptr++;
-    }
-}
-
 void function_one(vector<vector<int>> &graph, vector<int> &degree, int n, int m, float a, float b,map&revmp)
 {
     vector<int> newdeg = degree;
@@ -65,6 +52,16 @@ void function_two(vector<vector<int>> &edges, vector<int> &degree, int n, int m,
     int numOfNodes = revmp.size();
     int numOfEdgesRemoved = 0;
     float ratio1, ratio2;
+
+    ofstream ReportFile("report_oregon1_010526(output_f2_0.5-0.7).csv");
+
+    for(int i = 0; i<100 ; i++){
+
+        newdeg = degree;
+        int numOfEdgesRemoved = 0;
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    shuffle(edges.begin(), edges.end(), std::default_random_engine(seed));
 
     for (int i = 0; i < m-1; i++)
     {
@@ -118,7 +115,6 @@ void function_two(vector<vector<int>> &edges, vector<int> &degree, int n, int m,
     }
 
     // Print the Result
-    cout<<"\nDebug6.(b).."<<" $ ";
     // Create and open a text file
     ofstream MyFile("oregon1_010526(output_f2).txt");
 
@@ -138,16 +134,16 @@ void function_two(vector<vector<int>> &edges, vector<int> &degree, int n, int m,
             MyFile << "\nFor Vertex " << revmp[i] << " Old Degree : " << degree[i] << " New Degree : " << newdeg[i] << "\n";
             MyFile << "Fraction of vertex retained : " << ratio << "\n";
 
-            if(ratio > 0.7){
+            if(ratio > b){
                 fault_node_at_maxi++;
                 if(ratio > maxi_violation)  maxi_violation = ratio;
-                total_violation_upper += (ratio - 0.7);
+                total_violation_upper += (ratio - b);
             }
 
-            if(ratio < 0.5){
+            if(ratio < a){
                 fault_node_at_mini++;
                 if(ratio < mini_violation)  mini_violation = ratio;
-                total_violation_lower += (0.5 - ratio);
+                total_violation_lower += (a - ratio);
             }
         
         }
@@ -156,9 +152,11 @@ void function_two(vector<vector<int>> &edges, vector<int> &degree, int n, int m,
 
     float compression = (float)(m-numOfEdgesRemoved)/m;
     float density = (float)(m-numOfEdgesRemoved)/numOfNodes;
-    float avg_violation_upper = total_violation_upper/m;
-    float avg_violation_lower = total_violation_lower/m;
+    float avg_violation_upper = total_violation_upper/numOfNodes;
+    float avg_violation_lower = total_violation_lower/numOfNodes;
+    float avg_violation = (total_violation_lower + total_violation_upper)/numOfNodes;
     int fault_node = fault_node_at_mini + fault_node_at_maxi;
+    float percent_fault_node = (float)fault_node/numOfNodes*100;
 
     MyFile<<"\n\nOBSERVATIONS :::: ";
 
@@ -174,19 +172,31 @@ void function_two(vector<vector<int>> &edges, vector<int> &degree, int n, int m,
     MyFile<<"\nAverage Violation Lower Limit : "<<avg_violation_lower;
 
     MyFile.close();
+    // ReportFile<<", "<<compression<<", "<<density<<", "<<fault_node<<"\n";
+    ReportFile<<", "<<compression<<", "<<density<<", "<<fault_node<<", "<<percent_fault_node<<", "<<mini_violation<<", "<<maxi_violation<<", "<<avg_violation<<"\n";
+
+    }
+
+    cout<<"\nDebug6.(b).."<<" $ ";
+    ReportFile.close();
     
 }
 
-void function_three(vector<vector<int>> &edges, vector<int> &degree, int n, int m, float a, float b,map&revmp)
+void function_three(vector<vector<int>> &edges, vector<int>& degree, int n, int m, float a, float b,map&revmp)
 {
     vector<int> newdeg = degree;
     int u, v;
     float ratio1, ratio2;
     int numOfNodes = revmp.size();
-    cout<<"Number of Nodes in Graph: "<<numOfNodes<<"\n";
-    int numOfEdgesRemoved = 0;
+    // cout<<"Number of Nodes in Graph: "<<numOfNodes<<"\n";
     cout<<"Debug7(a).";
 
+    ofstream ReportFile("report_oregon1_010526(output_f3_0.5-0.7).csv");
+
+    for(int i = 0; i<100 ; i++){
+
+        newdeg = degree;
+        int numOfEdgesRemoved = 0;
     // Randomised the edges using Randomised sort function
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     shuffle(edges.begin(), edges.end(), std::default_random_engine(seed));
@@ -254,12 +264,8 @@ void function_three(vector<vector<int>> &edges, vector<int> &degree, int n, int 
             continue;
         }
 
-        // ratio1 = (float)newdeg[u]/degree[u];
-        // ratio2 = (float)newdeg[v]/degree[v];
-        // cout<<ratio1<<" , "<<ratio2<<"\n\n";
     }
 
-    cout<<"\nDebug7.(b).."<<" $ ";
     // Create and open a text file
     ofstream MyFile("oregon1_010526(output_f3).txt");
 
@@ -279,16 +285,16 @@ void function_three(vector<vector<int>> &edges, vector<int> &degree, int n, int 
             MyFile << "\nFor Vertex " << revmp[i] << " Old Degree : " << degree[i] << " New Degree : " << newdeg[i] << "\n";
             MyFile << "Fraction of vertex retained : " << ratio << "\n";
 
-            if(ratio > 0.7){
+            if(ratio > b){
                 fault_node_at_maxi++;
                 if(ratio > maxi_violation)  maxi_violation = ratio;
-                total_violation_upper += (ratio - 0.7);
+                total_violation_upper += (ratio - b);
             }
 
-            if(ratio < 0.5){
+            if(ratio < a){
                 fault_node_at_mini++;
                 if(ratio < mini_violation)  mini_violation = ratio;
-                total_violation_lower += (0.5 - ratio);
+                total_violation_lower += (a - ratio);
             }
         
         }
@@ -297,9 +303,11 @@ void function_three(vector<vector<int>> &edges, vector<int> &degree, int n, int 
 
     float compression = (float)(m-numOfEdgesRemoved)/m;
     float density = (float)(m-numOfEdgesRemoved)/numOfNodes;
-    float avg_violation_upper = total_violation_upper/m;
-    float avg_violation_lower = total_violation_lower/m;
+    float avg_violation_upper = total_violation_upper/numOfNodes;
+    float avg_violation_lower = total_violation_lower/numOfNodes;
+    float avg_violation = (total_violation_lower + total_violation_upper)/numOfNodes;
     int fault_node = fault_node_at_mini + fault_node_at_maxi;
+    float percent_fault_node = (float)fault_node/numOfNodes*100;
 
     MyFile<<"\n\nOBSERVATIONS :::: ";
 
@@ -315,7 +323,140 @@ void function_three(vector<vector<int>> &edges, vector<int> &degree, int n, int 
     MyFile<<"\nAverage Violation Lower Limit : "<<avg_violation_lower;
 
     MyFile.close();
+
+    ReportFile<<", "<<compression<<", "<<density<<", "<<fault_node<<", "<<percent_fault_node<<", "<<mini_violation<<", "<<maxi_violation<<", "<<avg_violation<<"\n";
+
+    }
+
+    cout<<"\nDebug7.(b).."<<" $ ";
+    ReportFile.close();
 }
+
+// ******** FUNCTION FOUR START ************* //
+
+void function_four(vector<vector<int>> &edges, vector<int> &degree, int n, int m, float a,unordered_map<int,int>& revmp)
+{
+    vector<int> newdeg = degree;
+    int u, v;
+    float ratio1, ratio2;
+    int numOfNodes = revmp.size();
+    cout<<"DEBUG Function FOUR \n";
+
+    ofstream ReportFile("report_oregon1_010526(output_f4_0.5-0.7).csv");
+
+    for(int itr = 0; itr<100 ; itr++){
+        newdeg = degree;
+        int numOfEdgesRemoved = 0;
+
+        // Randomised the edges using Randomised sort function
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    shuffle(edges.begin(), edges.end(), std::default_random_engine(seed));
+
+    for (int i = 0; i < m-1; i++)
+    {
+        u = edges[i][0];
+        v = edges[i][1];
+        // cout<<"\n"<<u<<" - "<<v<<"# ";
+
+        ratio1 = (float)newdeg[u] / degree[u];
+        ratio2 = (float)newdeg[v] / degree[v];
+        bool flag = true;
+
+        if (ratio1 > a)
+        {
+            if (ratio2 > a)
+            {
+                float r1 = ((float)newdeg[u] - 1) / degree[u];
+                float r2 = ((float)newdeg[v] - 1) / degree[v];
+                if (r1 >= a && r2 >= a)
+                {
+                    newdeg[u]--; // Edges U-V is removed when ratio1 > aplha
+                    newdeg[v]--; 
+                    numOfEdgesRemoved++;
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
+        else if (ratio1 > a)
+        { 
+            if (ratio2 > a)
+            {
+
+                float r1 = ((float)newdeg[u] - 1) / degree[u];
+                float r2 = ((float)newdeg[v] - 1) / degree[v];
+                if (r1 >= a && r2 >= a)
+                {
+                    newdeg[u]--; // Edges U-V is removed when ratio1 > aplha
+                    newdeg[v]--; 
+                    numOfEdgesRemoved++;
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
+        else
+        {
+            continue;
+        }
+
+    }
+
+    // cout<<"\nDebug8.(b).."<<" $ ";
+    // Create and open a text file
+    ofstream MyFile("oregon1_010526(output_f4).txt");
+
+    // Print the Result
+    int fault_node_at_maxi = 0;
+    int fault_node_at_mini = 0;
+    float mini_violation = a;
+    float total_violation_upper = 0.0;
+    float total_violation_lower = 0.0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if(degree[i] != 0){
+            float ratio = (float)newdeg[i] / degree[i];
+            MyFile << "\nFor Vertex " << i << " Old Degree : " << degree[i] << " New Degree : " << newdeg[i] << "\n";
+            MyFile << "Fraction of vertex retained : " << ratio << "\n";
+
+
+            if(ratio < a){
+                fault_node_at_mini++;
+                if(ratio < mini_violation)  mini_violation = ratio;
+                total_violation_lower += (a - ratio);
+            }
+        
+        }
+        // cout<<"Here 8$ ";
+    }
+
+    float compression = (float)(m-numOfEdgesRemoved)/m;
+    float density = (float)(m-numOfEdgesRemoved)/numOfNodes;
+    float avg_violation_lower = total_violation_lower/numOfNodes;
+    int fault_node = fault_node_at_mini + fault_node_at_maxi;
+
+    MyFile<<"\n\nOBSERVATIONS :::: ";
+
+    MyFile<<"\n\nDensity of Graph is reduced to : "<<compression;
+    MyFile<<"\nNumber of edges of removed : "<<numOfEdgesRemoved;
+
+    MyFile<<"\n\n Numberof Vertex violating on Lower Limit : "<<fault_node_at_mini;
+    MyFile<<"\nMinimum Violation of Lower Limit : "<<mini_violation;
+    MyFile<<"\nAverage Violation Lower Limit : "<<avg_violation_lower;
+
+    ReportFile<<", "<<compression<<", "<<density<<", "<<fault_node<<", "<<mini_violation<<", "<<avg_violation_lower<<"\n";
+
+    MyFile.close();
+    }
+    ReportFile.close();
+}
+
+// ******** FUNCTION FOUR END **************//
 
 int main()
 {
@@ -375,7 +516,7 @@ int main()
             m = num2;
             graph.resize(n);
             degree.resize(n,0);
-            cout<<"Code is Running  ";
+            // cout<<"Code is Running  ";
             
         }else{
 
@@ -413,17 +554,17 @@ int main()
     cout<<"\nMAXI : "<<maxi<<"#\n";
 
     
-    cout<<" map size "<<map1.size()<<" revmap size "<<revmp.size()<<endl;
-    cout<<" graph size "<<map1.size()<<" edge size "<<revmp.size()<<endl;
+    // cout<<" map size "<<map1.size()<<" revmap size "<<revmp.size()<<endl;
+    // cout<<" graph size "<<map1.size()<<" edge size "<<revmp.size()<<endl;
     cout << "\n"
-         << n << " vertex && " << m << " edges are there\n";
+         << revmp.size() << " vertex && " << m << " edges are there\n";
     // printGraph(graph);
 
     // Now DCS Problem let a = 50% bad b = 70% so we have to reduce the degree of each verted
     // between 50% to 70%.
 
     float a = 0.5, b = 0.7; // a = alpha , b = beeta
-
+    cout<<a<<" : alpha - Betta : "<<b<<"\n";
     // function_one(graph,degree,n,m,a,b);
 
     // ********* IMPLEMENTING THE SECOND FUNCTION ********** Following the UPPER BOUND STRICTLY
@@ -434,6 +575,11 @@ int main()
 
     cout<<"Debug7 $\n";
     function_three(edges,degree,n,m,a,b,revmp);
+
+    // ******** IMPLEMENTING THE FOURTH FUNCTION ********** ONLY LOWER BOUND CONSTRAINTS
+
+    cout<<"Debug9 $\n";
+    function_four(edges,degree,n,m,a,revmp);
 
     return 0;
 }
